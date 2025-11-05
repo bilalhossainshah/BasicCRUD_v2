b37256496ef82b47250dfb5d109aebcea296c724
Bilal Hussain <iambilalshah007@gmail.com>
2025-11-05 15:39:26 +0500

    Refactor main.py to include cart router
    Updated FastAPI app to include the cart router and removed unused imports.

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
-from fastapi import FastAPI,Depends
-from database import engine
-import models
-from routers import products,categorey,auth
-=======
 from fastapi import FastAPI
+from database import get_conn
+from routers import products,categorey,auth,cart
 # from database import engine
 # import models
-from routers import products,categorey
->>>>>>> 505d644 (code just .db or product,catagories wli file mn h)
 
 app = FastAPI()
 app.include_router(products.router)
 app.include_router(categorey.router)
-<<<<<<< HEAD
 app.include_router(auth.router)
-=======
+app.include_router(cart.router)
+
 # models.Base.metadata.create_all(bind = engine)
 # app.include_router(products.router)
 # app.include_router(categorey.router)
->>>>>>> 505d644 (code just .db or product,catagories wli file mn h)
 
